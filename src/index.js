b02539b84758ae08ac68f0c15656ce0454c36ea2
CodeSandbox <support@codesandbox.io>
2023-02-21 03:19:10 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,72 @@
+import "./styles.css";
+import { data } from "./skills-data";
+
+const visualization = document.getElementById("visualization");
+const rangeInput = document.getElementById("range");
+const minYear = Math.min(...data.map((d) => parseInt(Object.keys(d).slice(1))));
+const maxYear = Math.max(...data.map((d) => parseInt(Object.keys(d).slice(1))));
+rangeInput.min = minYear;
+rangeInput.max = maxYear;
+rangeInput.value = `${maxYear}`;
+
+function createVisualization(data, startYear, endYear) {
+  // Remove any columns that are already in the visualization
+  console.log("starting");
+  while (visualization.firstChild) {
+    visualization.removeChild(visualization.firstChild);
+  }
+
+  // Filter the data to include only the years within the given range
+  const filteredData = data.map((skillData) => {
+    const filteredYears = Object.keys(skillData).filter(
+      (key) =>
+        key !== "skill" &&
+        parseInt(key) >= startYear &&
+        parseInt(key) <= endYear
+    );
+    const filteredSkillData = { skill: skillData.skill };
+    filteredYears.forEach((year) => {
+      filteredSkillData[year] = skillData[year];
+    });
+
+    return filteredSkillData;
+  });
+
+  // Loop through the filtered data and create a column for each skill
+  console.log(filteredData);
+  filteredData.forEach((skillData) => {
+    const skill = skillData.skill;
+    const years = Object.keys(skillData).filter((key) => key !== "skill");
+    const column = document.createElement("div");
+    column.className = "column";
+
+    // Loop through the years and create a box for each instance of the skill
+    years.forEach((year) => {
+      if (skillData[year] > 0) {
+        const box = document.createElement("div");
+        box.className = "box";
+        column.appendChild(box);
+      }
+    });
+
+    // Add the skill name to the column
+    const label = document.createElement("div");
+    label.textContent = skill;
+    column.appendChild(label);
+
+    // Add the column to the visualization container
+    visualization.appendChild(column);
+  });
+}
+
+// Initial visualization with all years
+createVisualization(data, 2000, 2023);
+
+//Add event listener to the range input to update visualization on change
+
+// rangeInput.addEventListener("input", (event) => {
+//   //const startYear = parseInt(rangeInput.value.split(",")[0]);
+//   const endYear = parseInt(rangeInput.value.split(",")[1]);
+//   //createVisualization(data, startYear, endYear);
+//   createVisualization(data, 2015, 2011);
+// });
